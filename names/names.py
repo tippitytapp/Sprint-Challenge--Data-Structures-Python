e046a4f1256cb3780260870bde860cd542a1cf3b
Marc Tapp <marc.tapp@gmail.com>
2020-06-12 13:49:14 -0400

    got the stretch

--- names/names.py
+++ names/names.py
@@ -19,6 +19,7 @@ import time
 # f.close()
 
 # f = open('names_2.txt', 'r')
+
 # names_2 = f.read().split("\n")  # List containing 10000 names
 # f.close()
 
@@ -73,3 +74,19 @@ print(f"\ninitial runtime complexity: {initial_runtime_complexity}, final runtim
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+####### ----------------STRETCH--------------------- #######
+# start_time = time.time()
+
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+
+# duplicates = list(set(names_1)&set(names_2))
+# end_time = time.time()
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"initial runtime: {end_time - start_time} seconds") 
